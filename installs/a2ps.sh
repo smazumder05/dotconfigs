bc9d263478b875c862f39772540aada9a8a85bfb
smtechnocrat <smtechnocrat@gmail.com>
2014-12-14 22:44:44 -0500

    Initial commit.

---
+++ installs/a2ps.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+# 
+# Bash provisioning base script for my laptop.
+# 
+# The base script will install the following libs
+#
+# 1) a2ps GNU a2ps is an Any to PostScript filter. Of course it processes plain text files,
+#    but also pretty prints quite a few popular languages.
+#
+# Copyright@2014 smtechnocrat
+# Author: smtechnocrat
+# Send questions to smtechnocrat@gmail.com
+################################################################## 
+sudo apt-get install -y a2ps
+
