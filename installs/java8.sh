7032b0649a64aaaeb982447b8dc3059873ff5b40
smtechnocrat <smtechnocrat@gmail.com>
2014-12-30 01:08:03 -0500

    Added automated install options for jdk

--- installs/java8.sh
+++ installs/java8.sh
@@ -4,7 +4,10 @@
 #
 # The base script will install the following libs
 #
-# 1) oracle java-7
+# 1) oracle java-8
+#
+# Note: use sudo update-alternatives --config java 
+# in order to switch between multiple jdk versions
 #
 # Copyright@2014 smtechnocrat
 # Author: smtechnocrat
@@ -13,4 +16,4 @@
 sudo add-apt-repository -y ppa:webupd8team/java
 sudo apt-get update
 echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
-sudo apt-get -y install oracle-java7-installer
+sudo apt-get -y install oracle-java8-installer
