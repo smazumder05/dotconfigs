3659a8658d1c90792a6022efc04ba14916a2e7ce
Suvankar Mazumder <smazumder@gmail.com>
2017-03-07 10:23:02 -0500

    Update un-libreoffice.sh

--- scripts/un-libreoffice.sh
+++ scripts/un-libreoffice.sh
@@ -1,1 +1,6 @@
 #Remove libre-office and its components
+sudo apt-get remove --purge libreoffice*
+sudo apt-get clean
+sudo apt-get autoremove
+#Remove all fonts used by Libre
+sudo apt-get remove fonts-opensymbol libreoffice libreoffice-\* openoffice.org-dtd-officedocument1.0 python\*-uno uno-libs3-\* ure ure-dbg
