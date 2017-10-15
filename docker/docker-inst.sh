b442e55e8c1df86bad8e312ab5d5c8b80f753bfc
Suvankar Mazumder <smazumder@gmail.com>
2017-10-15 23:04:30 +0000

    Update docker-inst.sh

--- docker/docker-inst.sh
+++ docker/docker-inst.sh
@@ -4,6 +4,7 @@ sudo apt-get update
 sudo apt-get -y install docker.io
 sudo ln -sf /usr/bin/docker.io /usr/local/docker
 sudo sed -i '$acomplete -F _docker docker' /etc/bash_completion.d/docker.io
-sudo docker run -i -t ubuntu /bin/bash
 # add docker to current user group
 sudo usermod -aG docker $(whoami) #logout and try again
+
+sudo docker run -i -t ubuntu /bin/bash
