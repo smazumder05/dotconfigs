1c523df36d5ccb8431810071fbbf70a6a96ea059
Suvankar Mazumder <smazumder@gmail.com>
2017-10-15 18:56:31 -0400

    Update dock-utils-inst.sh

--- docker/dock-utils-inst.sh
+++ docker/dock-utils-inst.sh
@@ -1,1 +1,7 @@
-# Install docker utility tools
+#!/bin/bash
+
+sudo curl -L https://github.com/docker/compose/releases/download/1.16.1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+docker-compose --version
+
+
