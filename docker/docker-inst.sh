d084beda6c7996c276a9976c5c1bdc7ddf2dd7d7
Suvankar Mazumder <smazumder@gmail.com>
2017-10-15 23:01:36 +0000

    Update docker-inst.sh

--- docker/docker-inst.sh
+++ docker/docker-inst.sh
@@ -7,10 +7,3 @@ sudo sed -i '$acomplete -F _docker docker' /etc/bash_completion.d/docker.io
 sudo docker run -i -t ubuntu /bin/bash
 # add docker to current user group
 sudo usermod -aG docker $(whoami) #logout and try again
-
-#install dock for docker dev 
-curl https://raw.githubusercontent.com/bripkens/dock/master/dock -so ~/bin/dock && \
-     chmod +x ~/bin/dock && \
-     dock -u && \
-     echo "dock installation successful. Try running 'dock'"
-
